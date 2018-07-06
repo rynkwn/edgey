396128ee00192c0fa71c1ce1ba100063060e02d4
Ryan Kwon <rynkwn@gmail.com>
2018-07-05 22:12:37 -0700

    Starting some work.

--- src/processor.py
+++ src/processor.py
@@ -1,1 +1,33 @@
 from PIL import Image
+
+from tempfile import 
+
+def preprocessing(img):
+	# type: (Text) -> Image
+	"""
+	Arguments:
+		img (Text): The file location of the image.
+
+	Specifically, we:
+	1) Convert the image to Black and White.
+	"""
+	
+	im = Image.open(img).convert("L")
+	return im
+
+def processing():
+	pass
+
+def produce_edged_image(src, dest):
+	# type: (Text, Text) -> None
+	"""Creates an edged image at dest."""
+	im = preprocessing(src)
+	processing(im)
+
+def main(src, dest):
+	# type: (Text, Text) -> None
+	pass
+
+if __name__ == "__main__":
+	# main()
+	pass
